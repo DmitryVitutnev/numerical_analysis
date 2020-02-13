75316f24e8ce8fc56a71de71a6aa7f0cf9325f60
Дмитрий Витютнев <dimason99@gmail.com>
2020-02-13 21:59:32 +0600

    Invertion works

--- numerical_analysis/src/main/java/ru/omsu/imit/mgen/Inverter.java
+++ numerical_analysis/src/main/java/ru/omsu/imit/mgen/Inverter.java
@@ -2,7 +2,7 @@ package ru.omsu.imit.mgen;
 
 public class Inverter {
 
-    public void rotate(double[][] a, int n, int col, int i, int j) {
+    public void rotate(double[][] a, double[][] b, int n, int col, int i, int j) {
         double s;
         double c;
         s = -a[i][col]/Math.sqrt(a[i][col]*a[i][col]+a[j][col]*a[j][col]);
@@ -15,21 +15,86 @@ public class Inverter {
             a[j][k] = c * aj - s * ai;
             a[i][k] = s * aj + c * ai;
         }
+        a[i][col] = 0.;
+        double bj, bi;
+        for(int k = 0; k < n; k++) {
+            bj = b[j][k];
+            bi = b[i][k];
+
+            b[j][k] = c * bj - s * bi;
+            b[i][k] = s * bj + c * bi;
+        }
     }
 
-    public void triangulate(double[][] a, int n) {
+    public void triangulate(double[][] a, double[][] b, int n) {
         for(int j = 0; j < n-1; j++) {
             for(int i = j+1; i < n; i++) {
-                rotate(a, n, j, i, j);
+                rotate(a, b, n, j, i, j);
             }
         }
     }
 
+    public double[][] createIdentityMatrix(int n) {
+        double[][] ret = new double[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    ret[i][j] = 1.;
+                } else {
+                    ret[i][j] = 0.;
+                }
+            }
+        }
+        return ret;
+    }
+
+    public void invertTriangle(double[][] a, int n) {
+        double[] arr = new double[n];
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = i; j < n; j++) {
+                arr[j] = a[i][j];
+            }
+            a[i][i] = 1./arr[i];
+            for(int j = i+1; j < n; j++) {
+                a[i][j] = 0;
+                for(int k = i+1; k <= j; k++) {
+                    a[i][j] -= arr[k] * a[k][j];
+                }
+                a[i][j] /= arr[i];
+            }
+        }
+        /*for(int i = n-1; i >=0; i--) {
+            for(int j = 0; j < i; j++) {
+                a[i][j] = 0;
+            }
+        }*/
+    }
+
     public void invertMatrix(double[][] a, int n) {
 
+        Gen gen = new Gen();
+
+        double[][] b = createIdentityMatrix(n);
+
+        triangulate(a, b, n);
+
+        gen.print_matr(a, n);
+
+        invertTriangle(a, n);
+
+        gen.print_matr(a, n);
+
+        double[][] c = new double[n][n];
 
 
+        gen.matr_mul(a, b, c, n);
 
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                a[i][j] = c[i][j];
+            }
+        }
     }
 
 
--- numerical_analysis/src/test/java/ru/omsu/imit/mgen/GenTest.java
+++ numerical_analysis/src/test/java/ru/omsu/imit/mgen/GenTest.java
@@ -38,16 +38,6 @@ public class GenTest
 	@Test
 	public void InvertTest()
 	{
-		int n = N;
-		double alpha = ALPHA;
-		double beta  = BETA;
-
-		double[][] a = new double[n][];
-		for (int i = 0; i < n; i++)	a[i] = new double[n];
-
-		double[][] a_inv = new double[n][];
-		for (int i = 0; i < n; i++)	a_inv[i] = new double[n];
-
 		Gen g = new Gen();
 
 		//g.mygen ( a, a_inv, n, alpha, beta, 1, 2, 0, 1 ); // симметричная
@@ -55,14 +45,19 @@ public class GenTest
 		//	g.mygen ( a, a_inv, n, alpha, beta, 0, 0, 2, 1 ); //жорданова клетка
 
 		double[][] arr = {{1,2,3},{4,5,6},{7,8,10}};
+		double[][] arrInv = {{1,2,3},{4,5,6},{7,8,10}};
 
 		g.print_matr(arr,3);
 
 
 		Inverter inverter = new Inverter();
-		inverter.triangulate(arr,3);
+		inverter.invertMatrix(arr,3);
 
 		g.print_matr(arr,3);
+
+		double[][] identity = new double[3][3];
+		g.matr_mul(arr, arrInv, identity, 3);
+		g.print_matr(identity,3);
 	}
 
 }
